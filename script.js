6517d5e877855b83f62cc092fb72c3987d35d547
Daniela Coltri Fernandes <danicoltri@gmail.com>
2023-09-04 10:15:29 -0300

    Ajustes do responsivo + textos

--- script.js
+++ script.js
@@ -49,3 +49,16 @@ const textArea2 = document.querySelector(".msg");
 
 btnCopiar.addEventListener("click", (e) => {
     navigator.clipboard.writeText(textArea2.value);});
+
+
+// /*Função que some com a imagem e o texto do text area msg*/
+// var x = "Mozilla";
+// var empty = "";
+
+// console.log(
+//   "Mozilla possui " + x.length + " unidades de código de comprimento",
+// );
+// /* "Mozilla possui 7 unidades de código de comprimento" */
+
+// console.log("A string vazia possui um comprimento de " + empty.length);
+// /* "A string vazia possui um comprimento de 0" */
